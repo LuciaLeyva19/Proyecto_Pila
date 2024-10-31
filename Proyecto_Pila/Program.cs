da5569bc52966686f31f964cd4c26070e66f041c
lucia <lucia@LUCIALEYVA123>
2024-10-31 11:40:50 -0700

    Agregar archivos de proyecto.

---
+++ Proyecto_Pila/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Pila
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            pila stack = null;
+            bool exit = false;
+
+            while (!exit)
+            {
+                Console.Clear();
+                Console.WriteLine("MENU Pilas");
+                Console.WriteLine("---------------------------------");
+                Console.WriteLine("1.- Establecer Tamaño");
+                Console.WriteLine("2.- Push");
+                Console.WriteLine("3.- Pop");
+                Console.WriteLine("4.- Imprimir");
+                Console.WriteLine("5.- Salir");
+                Console.WriteLine("---------------------");
+                Console.Write("Seleccionar Opción => ");
+
+                int opcion = int.Parse(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case 1:
+                        Console.Write("Ingrese el tamaño de la pila: ");
+                        int max = int.Parse(Console.ReadLine());
+                        if (stack == null)
+                        {
+                            stack = new pila(max);
+                            Console.WriteLine("Tamaño de la pila establecido.");
+                        }
+                        else
+                        {
+                            stack.EstablecerTamano(max);
+                        }
+                        break;
+
+                    case 2:
+                        if (stack == null)
+                        {
+                            Console.WriteLine("Primero debe establecer el tamaño de la pila.");
+                        }
+                        else
+                        {
+                            Console.Write("Ingrese el número a insertar: ");
+                            int num = int.Parse(Console.ReadLine());
+                            if (stack.Push(num))
+                                Console.WriteLine("Elemento insertado correctamente.");
+                            else
+                                Console.WriteLine("La pila está llena. No se pudo insertar.");
+                        }
+                        break;
+
+                    case 3:
+                        if (stack == null)
+                        {
+                            Console.WriteLine("Primero debe establecer el tamaño de la pila.");
+                        }
+                        else
+                        {
+                            int popValue = stack.Pop();
+                            if (popValue == -1)
+                                Console.WriteLine("La pila está vacía.");
+                            else
+                                Console.WriteLine($"Elemento extraído: {popValue}");
+                        }
+                        break;
+
+                    case 4:
+                        if (stack == null)
+                        {
+                            Console.WriteLine("Primero debe establecer el tamaño de la pila.");
+                        }
+                        else
+                        {
+                            stack.PrintStack();
+                        }
+                        break;
+
+                    case 5:
+                        exit = true;
+                        Console.WriteLine("Saliendo del programa...");
+                        break;
+
+                    default:
+                        Console.WriteLine("Opción no válida.");
+                        break;
+                }
+                Console.WriteLine("Presione cualquier tecla para continuar...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
+
---
+++ Proyecto_Pila/pila.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Pila
+{
+    internal class pila
+    {
+        private int MAX;
+        private int tope = 0;
+        private nodo inicio;
+
+        public pila (int max)
+        {
+            MAX = max;
+            inicio = null;
+        }
+        
+        public bool Empty()
+        {
+            if (inicio == null)
+                return true;
+            else
+                return false;
+        }
+
+        public bool Full()
+        {
+            if (tope == MAX) 
+                return true;
+            else
+            {
+                return false;
+            }
+        }
+
+        public void EstablecerTamano(int cantidad)
+        {
+            if (cantidad <= 0)
+            {
+                Console.WriteLine("La cantidad a agregar debe ser mayor que 0");
+                return;
+            }
+
+            MAX += cantidad;
+            Console.WriteLine($"Capacidad de la pila incrementada. Nuevo tope máximo: {MAX}");
+ 
+        }
+
+        public void PrintStack()
+        {
+            nodo act = inicio;
+
+            Console.WriteLine("Elementos de la pila:");
+            while (act != null)
+            {
+                Console.WriteLine(act.Valor);
+                act = act.Sig;
+            }
+            //Imprime la pila
+        }
+
+        public bool Push(int num)
+        {
+
+            if (Full())
+                return false;
+
+            nodo nuevo = new nodo(num);
+            nuevo.Sig = inicio;
+            inicio = nuevo;
+            tope++;
+            return true;
+            //Regresa true si la inserción es exitosa 
+            //Regresa false si la pila esta llena y no se insertó
+            //No muestra mensajes por dentro
+        }
+
+        public int Pop()
+        {
+            if (Empty())
+                return -1;
+
+            int valor = inicio.Valor;
+            inicio = inicio.Sig;
+            tope--;
+            return valor;
+            //Regresa el numero sacado de la pila
+            //Si la pila esta vacia regresa -1
+        }
+
+
+    }
+}
